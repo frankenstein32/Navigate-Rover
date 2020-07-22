1041d16988da86a2bbf99daf7f1801cbe66c4a66
frankenstein32 <saifirashid32@gmail.com>
2020-07-22 16:40:18 +0530

    Grid Added

--- Versions/script.js
+++ Versions/script.js
@@ -1,14 +1,12 @@
 function plot(rows = 50, cols = 50) {
+  let c = "", y = 0;
     for (let i = 0; i < rows; i++) {
       var x = 0;
       for (let j = 0; j < cols; j++) {
         let colr = "rgb(255, 255, 255)";
-        if (j == 0 || i == 0 || j == cols - 1 || i == rows - 1) {
-          colr = "rgb(43, 45, 47) ";
-        }
         c += `<rect id=${
           i + ":" + j
-        } x="${x}" y="${y}" class="button" onClick=reply_click(this) width="30" height="30" r="0" rx="0" ry="0" fill="${colr}" stroke="#000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-opacity: 0.2;" stroke-opacity="0.2"></rect>`;
+        } x="${x}" y="${y}" width="30" height="30" r="0" rx="0" ry="0" fill="${colr}" stroke="#000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-opacity: 0.2;" stroke-opacity="0.2"></rect>`;
         x += 30;
       }
       y += 30;
